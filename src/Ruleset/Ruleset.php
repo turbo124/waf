edb7386527973b7e581a96df7ed81e28657dec27
David Bomba <turbo124@gmail.com>
2024-02-25 15:47:03 +1100

    Update requirements for package

--- src/Ruleset/Ruleset.php
+++ src/Ruleset/Ruleset.php
@@ -50,5 +50,148 @@ class Ruleset
         throw new \Exception("Could not get rules " . $response->error());
 
     }
+
+    /**
+     * Adds a rule from the Firewall
+     *
+     * @param  string $expression
+     * @param  string $action
+     * 
+     * @return bool
+     */
+    public function addRule(string $expression, string $action)
+    {
+        $ruleset = $this->getRuleset();
+
+        $rule = false;
+        
+        if(isset($ruleset['rules'])) {
+            $rule = collect($ruleset['rules'])->first(function ($rules) use ($action) {
+                return $rules['action'] == $action;
+            });
+        }
+
+        if($rule) {
+            return $this->updateRuleExpression($ruleset, $this->addExpression($rule, $expression), $rule);
+        }
+
+        return $this->addRuleParent($ruleset, $expression, $action);
+    }
     
+
+
+    /**
+     * Updates the rules expression
+     *
+     * @param  array $ruleset
+     * @param  string $expression
+     * @param  array $rule
+     * 
+     * @return bool
+     */
+    public function updateRuleExpression(array $ruleset, string $expression, array $rule): bool
+    {
+        $rule['expression'] = $expression;
+
+        if(strlen($expression) == 0)
+            return $this->deleteRule($ruleset, $rule);
+
+        $cloudflare_endpoint = "{$this->waf->url}zones/{$this->waf->zone_id}/rulesets/{$ruleset['id']}/rules/{$rule['id']}";
+
+        
+        $response = $this->waf->client->request($cloudflare_endpoint, "PATCH", $rule);
+
+        if($response->successful()) {
+            return true;
+        }
+
+        throw new \Exception("Could not get rules " . $response->body());
+
+    }
+    
+    /**
+     * Adds a expression to the rule
+     *
+     * @param  array $rule
+     * @param  string $expression
+     * 
+     * @return string
+     */
+    public function addExpression($rule, $expression): string
+    {
+        
+        return collect(explode("or", $rule['expression']))->filter(function ($current) use ($expression) {
+            return $current != $expression;
+        })->push($expression)->implode("or");
+
+    }
+    
+    /**
+     * Removes a expression to the rule
+     *
+     * @param  array $rule
+     * @param  string $expression
+     * 
+     * @return string
+     */
+    public function removeExpression($rule, $expression)
+    {
+        
+        return collect(explode("or", $rule['expression']))->filter(function ($current) use ($expression) {
+            return $current != $expression;
+        })->implode("or");
+
+    }
+    
+     public function deleteRule($ruleset, $rule) {
+
+        $ruleset_id = $ruleset['id'];
+        $rule_id = $rule['id'];
+
+        $cloudflare_endpoint = "{$this->waf->url}zones/{$this->waf->zone_id}/rulesets/{$ruleset_id}/rules/{$rule_id}";
+
+        $response = $this->waf->client->request($cloudflare_endpoint, "DELETE", []);
+
+        if($response->successful()) {
+            return true;
+        }
+
+        throw new \Exception("Could not get rules " . $response->body());
+
+    }
+
+
+    /**
+     * Adds a new rule
+     *
+     * @param  array $ruleset
+     * @param  string $expression
+     * @param  string $action
+     * 
+     * @return bool
+     */
+    public function addRuleParent(array $ruleset, string $expression, string $action)
+    {
+
+        $cloudflare_endpoint = "{$this->url}zones/{$this->getZone()}/rulesets/{$ruleset['id']}/rules";
+
+        $rule = [
+            'action' => $action,
+            'expression' => $expression,
+            'description' => "Added by botlicker on " . \Carbon\Carbon::now()->toDateTimeString()
+        ];
+
+        $response =
+        
+        $response = $this->waf->client->request($cloudflare_endpoint, "POST", $rule);
+
+        if($response->successful()) {
+            return true;
+        }
+
+        throw new \Exception("Could not add rule {$action} => " . $response->body());
+
+    }
+
+
 }
